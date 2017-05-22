b0bc86f136b9137b4b562b0c3193d6a62c2306fd
linchpin <linchpin@linchpin-Vostro-3559>
2017-05-22 20:25:08 +0530

    session identifier class

---
+++ Test/src/SessionIdentifierGenerator.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class SessionIdentifierGenerator {
+	
+	private SecureRandom random = new SecureRandom();
+
+	  public String nextSessionId() {
+	    return new BigInteger(130, random).toString(32);
+	  }
+
+	  
+	  
+	  public static void main(String args[])
+	  {
+		  SessionIdentifierGenerator gen =  new SessionIdentifierGenerator();
+		  System.out.println(gen.nextSessionId());
+		  System.out.println(gen.nextSessionId());
+		  System.out.println(gen.nextSessionId());
+		  System.out.println(gen.nextSessionId());
+	  }
+}
